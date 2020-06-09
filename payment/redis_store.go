8faf246131783fce70bf83657f6794194f4e8ffb
srinathjayaraman <srinath.msd@gmail.com>
2020-06-09 22:26:35 +0200

    incorporated changes suggested by Martijn

--- payment/redis_store.go
+++ payment/redis_store.go
@@ -2,11 +2,8 @@ package payment
 
 import (
 	"fmt"
-	//"strconv"
 	"strings"
-	//"encoding/json"
 	"github.com/go-redis/redis/v8"
-	//"github.com/gofrs/uuid"
 	"github.com/martijnjanssen/redi-shop/util"
 	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
@@ -17,22 +14,20 @@ type redisPaymentStore struct {
 	urls *util.Services
 }
 
-func newRedisPaymentStore(c *redis.Client) *redisPaymentStore {
+func newRedisPaymentStore(c *redis.Client, urls *util.Services) *redisPaymentStore {
 	// AutoMigrate structs to create or update database tables
 	return &redisPaymentStore{
 		store: c,
+		urls: urls,
 	}
 }
 
 func (s *redisPaymentStore) Pay(ctx *fasthttp.RequestCtx, userID string, orderID string, amount int) {
 
-	var json string
-	
 	//Call the user service to subtract the order amount from the users' credit
 
 	c := fasthttp.Client{}
-	status, _, err := c.Post([]byte{}, fmt.Sprintf("http://%s:8000/users/credit/subtract/%s/%d", s.urls.User, userID, amount), nil)
-
+	status, _, err := c.Post([]byte{}, fmt.Sprintf("%s/users/credit/subtract/%s/%d", s.urls.User, userID, amount), nil)
 	if err != nil {
 		logrus.WithError(err).Error("unable to subtract credit")
 		util.InternalServerError(ctx)
@@ -42,10 +37,10 @@ func (s *redisPaymentStore) Pay(ctx *fasthttp.RequestCtx, userID string, orderID
 		ctx.SetStatusCode(status)
 		return
 	} 
-		json = fmt.Sprintf("{\"amount\": %d, \"status\": \"paid\"}", amount)
+	
+	json := fmt.Sprintf("{\"amount\": %d, \"status\": \"paid\"}", amount)
 
 	//Set payment status to paid. SETNX command will set key to hold a string value if key does not exist. If key already exists, no operation is performed.
-
 	set := s.store.SetNX(ctx, orderID, json, 0)
 	if set.Err() != nil {
 		logrus.WithError(set.Err()).Error("payment unsuccessful")
@@ -56,7 +51,6 @@ func (s *redisPaymentStore) Pay(ctx *fasthttp.RequestCtx, userID string, orderID
 }
 
 func (s *redisPaymentStore) Cancel(ctx *fasthttp.RequestCtx, userID string, orderID string) {
-
 	// Retrieve the payment which needs to be cancelled
 	get := s.store.Get(ctx, orderID)
 	if get.Err() == redis.Nil {
@@ -68,20 +62,18 @@ func (s *redisPaymentStore) Cancel(ctx *fasthttp.RequestCtx, userID string, orde
 		return
 	}
 
-	json := get.Val() //get the amount and status in this format --> {amount: int, status: string}
+	json := get.Val() //get the amount and status in this format --> {amount: int, status: "string"}
 	jsonSplit := strings.Split(json, ": ") //split the json above on the ":". so now jsonSplit is an array of 3 elements - "{amount" "int,status" "string}"
 
-	//code for retrieving only the status of the payment from the json
+	//code for retrieving only the status of the payment from the json (used to check if the payment has already been cancelled)
 
-	paymentString := jsonSplit[2] //this will access the 3rd element of the jsonSplit string which is "string }" [we don't need the "}"]
-	payment_status := paymentString[0 : len(paymentString)-1]
+	payment_status := jsonSplit[2][0 : len(jsonSplit[2])-1] //this will access the 3rd element of the jsonSplit string which is "string }" ["}" not needed]
 
 	//code for retrieving only the amount of the payment from the json (used to refund credit to the user)
 	
-	amountString := jsonSplit[1] //this will access the 2nd element of the jsonSplit string which is "int, status" [we only need the int]
-	amount := amountString[0 : len(amountString)-2] 
+	amount := jsonSplit[1][0 : len(jsonSplit[1])-2] //this will access the 2nd element of the jsonSplit string which is "int, status" [we only need the int] 
 
-	if payment_status == "cancelled" {
+	if payment_status == "\"cancelled\"" {
 		logrus.Info("payment is already cancelled")
 		util.BadRequest(ctx)
 		return
@@ -89,13 +81,12 @@ func (s *redisPaymentStore) Cancel(ctx *fasthttp.RequestCtx, userID string, orde
 	
 	// Refund the credit to the user
 	c := fasthttp.Client{}
-	status, _, err := c.Post([]byte{}, fmt.Sprintf("http://%s:8000/users/credit/add/%s/%d", s.urls.User, userID, amount), nil)
+	status, _, err := c.Post([]byte{}, fmt.Sprintf("%s/users/credit/add/%s/%d", s.urls.User, userID, amount), nil)
 	if err != nil {
 		logrus.WithError(err).Error("unable to refund credit to user")
 		util.InternalServerError(ctx)
 		return
-	}
-	if status != fasthttp.StatusOK {
+	} else if status != fasthttp.StatusOK {
 		logrus.WithField("status", status).Error("error while refunding credit to user")
 		ctx.SetStatusCode(status)
 		return
@@ -112,7 +103,6 @@ func (s *redisPaymentStore) Cancel(ctx *fasthttp.RequestCtx, userID string, orde
 }
 
 func (s *redisPaymentStore) PaymentStatus(ctx *fasthttp.RequestCtx, orderID string) {
-
 	get := s.store.Get(ctx, orderID)
 	if get.Err() == redis.Nil {
 		util.NotFound(ctx)
